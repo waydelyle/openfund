deaaa8f12f647b78b1ae1c46d106f1f5e48d25f2
Wayde Lyle <wayde.lyle@double-eye.com>
2016-04-12 12:58:12 +0200

    Changed bootstrap.css and minified bootstrap files. Changed navigation bar. Added openfund config.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,50 +2,37 @@
 
 @section('content')
 
-<!-- Navigation -->
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
+        <!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">OpenFund</a>
+            <a class="navbar-brand" href="#">Start Bootstrap</a>
         </div>
-
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
                 </li>
             </ul>
-            <form class="navbar-form navbar-left" role="search">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Register</a></li>
-            </ul>
         </div>
+        <!-- /.navbar-collapse -->
     </div>
+    <!-- /.container -->
 </nav>
-<!-- End Navigation -->
 
 <!-- Page Content -->
 <div class="container">
@@ -53,7 +40,7 @@
     <div class="row">
 
         <div class="col-md-3">
-            <p class="lead">OpenFund</p>
+            <p class="lead">Shop Name</p>
             <div class="list-group">
                 <a href="#" class="list-group-item active">Category 1</a>
                 <a href="#" class="list-group-item">Category 2</a>
