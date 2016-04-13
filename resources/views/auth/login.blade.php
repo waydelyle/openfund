a1667b91035f46e9a24b98617e4f9672fe7cac59
Brendan <brendan.vandermeulen@double-eye.com>
2016-04-13 12:31:23 +0200

    login and registration view changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,37 @@
 @extends('layouts.master')
 
 @section('content')
+<!-- resources/views/auth/login.blade.php -->
+<div class="container">
+    <div class="col-lg-10">
+        <form class="form-horizontal" method="POST" action="/auth/login">
+            <fieldset>
+                <legend align="center">Log In</legend>
+                {!! csrf_field() !!}
 
-    <!-- resources/views/auth/login.blade.php -->
+                <div class="form-group">
+                    <label for="email" class="col-lg-2 control-label">Email</label>
+                    <div class="col-lg-10">
+                        <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}">
+                    </div>
+                </div>
 
-    <form method="POST" action="/auth/login">
-        {!! csrf_field() !!}
+                <div class="form-group">
+                    <label for="password" class="col-lg-2 control-label">Password</label>
+                    <div class="col-lg-10">
+                        <input type="password" class="form-control" id="password" name="password" id="password">
+                    </div>
+                </div>
 
-        <div>
-            Email
-            <input type="email" name="email" value="{{ old('email') }}">
-        </div>
+                <div class="form-group" align="center">
+                    <input type="checkbox" name="remember"> Remember Me
+                </div>
 
-        <div>
-            Password
-            <input type="password" name="password" id="password">
-        </div>
-
-        <div>
-            <input type="checkbox" name="remember"> Remember Me
-        </div>
-
-        <div>
-            <button type="submit">Login</button>
-        </div>
-    </form>
+                <div class="form-group" align="center">
+                    <button type="submit">Login</button>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+</div>
 @endsection
