c624334079612b136e525375af16dfc671fe7649
Wayde Lyle <wayde.lyle@double-eye.com>
2016-05-04 15:32:20 +0200

    Fixed missing facade error.

--- app/Facades/FormModule.php
+++ app/Facades/FormModule.php
@@ -1,6 +1,6 @@
 <?php namespace App\Facades;
 use Illuminate\Support\Facades\Facade;
 
-class ProjectModule extends Facade{
-    protected static function getFacadeAccessor() { return \App\Modules\ProjectModule::class; }
+class FormModule extends Facade{
+    protected static function getFacadeAccessor() { return \App\Modules\FormModule::class; }
 }
--- app/Facades/ProjectModule.php
+++ app/Facades/ProjectModule.php
@@ -1,6 +1,6 @@
 <?php namespace App\Facades;
 use Illuminate\Support\Facades\Facade;
 
-class FormModule extends Facade{
-    protected static function getFacadeAccessor() { return \App\Modules\FormModule::class; }
+class ProjectModule extends Facade{
+    protected static function getFacadeAccessor() { return \App\Modules\ProjectModule::class; }
 }
