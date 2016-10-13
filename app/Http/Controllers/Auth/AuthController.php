44e10ab8bc6c6b6098e910f15086fe7f835227c4
Aristheo B. Lacdao <aceraven777@gmail.com>
2016-10-13 16:54:51 +0800

    Fix bug in logout
    In your routes file, you explicitly specify the logout route to the getLogout function, so you have to explicitly define it to the exception to guest middleware

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -39,7 +39,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+        $this->middleware($this->guestMiddleware(), ['except' => 'getLogout']);
     }
 
     /**
